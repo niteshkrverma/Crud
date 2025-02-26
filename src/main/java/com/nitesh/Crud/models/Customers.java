9ad262cfab6307e258581ecfa7ff2e32d64c2d20
nitesh verma <nverma@vitwo.in>
2025-02-26 10:40:58 +0530

    Customer API With image

--- src/main/java/com/nitesh/Crud/models/Customers.java
+++ src/main/java/com/nitesh/Crud/models/Customers.java
@@ -14,67 +14,42 @@ public class Customers {
     private String email;
     private String pwd;
     private long number;
+    private String image; // Store image filename
 
     // Default constructor
     public Customers() {}
 
     // Parameterized constructor
-    public Customers(String name, String address, String email, String pwd, long number) {
+    public Customers(String name, String address, String email, String pwd, long number, String image) {
         this.name = name;
         this.address = address;
         this.email = email;
         this.pwd = pwd;
         this.number = number;
+        this.image = image;
     }
 
     // Getters and Setters
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getAddress() {
-        return address;
-    }
+    public String getPwd() { return pwd; }
+    public void setPwd(String pwd) { this.pwd = pwd; }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public long getNumber() { return number; }
+    public void setNumber(long number) { this.number = number; }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPwd() {
-        return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
-
-    public long getNumber() {
-        return number;
-    }
-
-    public void setNumber(long number) {
-        this.number = number;
-    }
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
 
     @Override
     public String toString() {
@@ -85,6 +60,7 @@ public class Customers {
                 ", email='" + email + '\'' +
                 ", pwd='" + pwd + '\'' +
                 ", number=" + number +
+                ", image='" + image + '\'' +
                 '}';
     }
 }
